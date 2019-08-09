5836010a0216bb66d2f2d500545589739d67400c
Ghait <ghait@mogic.com>
2019-08-09 08:44:30 +0200

    update customer service for data transfer to partner

--- src/main/java/com/example/customerservice/service/CustomerService.java
+++ src/main/java/com/example/customerservice/service/CustomerService.java
@@ -1,34 +1,48 @@
 package com.example.customerservice.service;
 
 import com.example.customerservice.dto.CustomerDto;
-import com.example.customerservice.dto.UserDto;
+import com.example.customerservice.dto.CustomerGlobalInformation;
+import com.example.customerservice.dto.CustomerList;
+import com.example.customerservice.dto.PartnerList;
 import com.example.customerservice.entity.Customer;
 import com.example.customerservice.error.UserAlreadyExistException;
-import com.example.customerservice.repository.CostumerRepository;
-import com.squareup.okhttp.*;
+import com.example.customerservice.repository.CustomerRepository;
+import com.example.customerservice.repository.PartnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 public class CustomerService implements ICustomerService {
 
     @Autowired
-    CostumerRepository costumerRepository;
+    CustomerRepository costumerRepository;
+
+    @Autowired
+    OAuth2RestTemplate restTemplate;
+
+    @Autowired
+    PartnerRepository partnerRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
     @Override
-    public Customer registerNewCustomerAccount(CustomerDto accountDto) throws UserAlreadyExistException, IOException {
-        if (emailExists(accountDto.getEmail())) {
-            throw new UserAlreadyExistException("There is an account with that email adress: " + accountDto.getEmail());
+    public Customer registerNewCustomerAccount(CustomerDto customerForm) throws UserAlreadyExistException, IOException {
+        if (emailExists(customerForm.getEmail())) {
+            throw new UserAlreadyExistException("There is an account with that email adress: " + customerForm.getEmail());
         }
         final Customer customer = new Customer();
-        customer.setVorname(accountDto.getVorname());
-        customer.setNachname(accountDto.getNachname());
-        customer.setEmail(accountDto.getEmail());
-        customer.setTelNumbre(accountDto.getTelNumbre());
+        customer.setEmail(customerForm.getEmail());
 
-        OkHttpClient client = new OkHttpClient();
+        /*OkHttpClient client = new OkHttpClient();
 
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(mediaType, "{" +
@@ -43,7 +57,7 @@ public class CustomerService implements ICustomerService {
                 .addHeader("Content-Type", "application/json")
                 .build();
 
-        client.newCall(request).execute();
+        client.newCall(request).execute();*/
         //restTemplate.postForObject("http://user-service/registration", user,String.class);
         //OAuth2AccessToken oAuth2AccessToken = restTemplate.getAccessToken();
         return costumerRepository.save(customer);
@@ -52,6 +66,39 @@ public class CustomerService implements ICustomerService {
         return costumerRepository.findByEmail(email) != null;
     }
 
+    public CustomerList getCustomersByIds(List<Long> customerIds){
+        List<Customer> customers = new ArrayList<>();
+        customerRepository.findAllById(customerIds).forEach(customer -> {
+            customers.add(customer);
+        });
 
+        CustomerList customerList = new CustomerList(customers);
+
+        return customerList;
+    }
+
+    public PartnerList getAllPartnersforCustomer(Long customerId){
+
+        List<Long> partnerIds = new ArrayList<>();
+        partnerRepository.findByCustomerId(customerId).forEach(customerPartner -> {
+            partnerIds.add(customerPartner.getPartnerId());
+        });
+        PartnerList partners = restTemplate.postForObject("http://partner-service/getpartnersbyidlist",partnerIds,PartnerList.class);
+
+        return partners;
+    }
+
+    public Optional<Customer> getCustomerById(Long customerId){
+        return customerRepository.findById(customerId);
+    }
+
+    public CustomerGlobalInformation getCustomerGlobalInfosById(Long cutomerId){
+        //Todo get partner (principal user) sector
+        String partnerSector="Friseur";
+
+        String url = "http://" + partnerSector.toLowerCase() +"-information-service/globalinfos/" + cutomerId;
+        return restTemplate.getForObject(url,CustomerGlobalInformation.class);
+
+    }
 
 }
