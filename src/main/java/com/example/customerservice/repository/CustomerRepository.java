b6a1a6de41b36136fcff0346aaa89435a946daf8
Ghait <ghait@mogic.com>
2019-08-09 08:43:20 +0200

    update cutomer repo for transfer of data to cutomer data to partner service

--- src/main/java/com/example/customerservice/repository/CustomerRepository.java
+++ src/main/java/com/example/customerservice/repository/CustomerRepository.java
@@ -4,9 +4,14 @@ import com.example.customerservice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface CostumerRepository extends JpaRepository<Customer,Long> {
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
     Customer findByEmail(String email);
+
+
+    @Override
+    Optional<Customer> findById(Long id);
     //List<Customer> findAllByParnterId(Long partnerId);
 
 }
